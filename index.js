8d973689daea482223c7aeb600a24767a44c7f60
Angel Gurrola <angelgurrola2@gmail.com>
2026-01-21 14:52:53 -0800

    Add functional search methods using POST method on /add route. return search results

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import pg from "pg";
 import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let dummyBooks = [{
@@ -27,12 +28,39 @@ const db = new pg.Client({
     port: 5432
 });
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.render("index.ejs", { books: dummyBooks });
 });
 
-app.get("/add", (req,res) => {
+app.get("/add", (req, res) => {
     res.render("add.ejs");
+});
+
+app.post("/add", async (req, res) => {
+    let URL = "https://openlibrary.org/search.json?";
+    const searchType = req.body.searchType;
+    const searchValue = req.body.query;
+
+    URL = URL + `${searchType}=${searchValue}&limit=10`;
+    // console.log(URL);
+
+    try {
+        const result = await axios.get(URL)
+        const books = result.data.docs.map(book => ({
+            title: book.title,
+            author: book.author_name?.[0] || "Unknown",
+            cover: book.cover_i ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
+                : "/images/no-cover.png",
+            year: book.first_publish_year
+        }));
+        // console.log(books);
+        return res.render("add.ejs", { results: books });
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+    res.render("add.ejs")
 });
 
 app.listen(port, () => {
