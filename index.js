094915a649d5d56dfb13997ee01eb71b43493895
Angel Gurrola <angelgurrola2@gmail.com>
2026-01-26 22:49:31 -0800

    create info.ejs which can be accessed by clicking on the book card on the home page, All the info is displayed here, next session would add the Update and delete to the home page and also the /info page.

--- index.js
+++ index.js
@@ -33,9 +33,53 @@ const db = new pg.Client({
 
 db.connect();
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     saveData = {};
-    res.render("index.ejs", { books: dummyBooks });
+
+    const result = await db.query(`SELECT * FROM books, images, notes, opinions
+        WHERE books.id = images.book_id
+        AND books.id = notes.book_id
+        AND books.id = opinions.book_id`
+    );
+
+    res.render("index.ejs", { books: result.rows });
+});
+
+app.get("/info/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+        const result = await db.query(`
+            SELECT 
+                b.id,
+                b.title,
+                b.author,
+                b.publisher,
+                b.publication,
+                b.page_count,
+                b.isbn,
+                i.book_img,
+                o.comment,
+                o.score,
+                o.date_read,
+                n.content AS notes
+            FROM books b
+            LEFT JOIN images i ON i.book_id = b.id
+            LEFT JOIN opinions o ON o.book_id = b.id
+            LEFT JOIN notes n ON n.book_id = b.id
+            WHERE b.id = $1
+            `,
+            [id]
+        );
+
+        if(result.rows.length === 0){
+            return res.status(404).send("book not found");
+        }
+
+        res.render("info.ejs", { book: result.rows[0] });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Server Error");
+    }
 });
 
 app.get("/add", (req, res) => {
@@ -103,49 +147,49 @@ app.get("/save", async (req, res) => {
 
 app.post("/save", async (req, res) => {
     try {
-    saveData.comment = req.body.impression;
-    saveData.notes = req.body.notes;
-    saveData.date_read = req.body.data_read;
-    saveData.score = req.body.score;
-    console.log(saveData);
+        saveData.comment = req.body.impression;
+        saveData.notes = req.body.notes;
+        saveData.date_read = req.body.data_read;
+        saveData.score = req.body.score;
+        console.log(saveData);
 
-    const bookResult = await db.query(
-      `INSERT INTO books (title, author, publisher, publication, page_count, work_key, isbn)
+        const bookResult = await db.query(
+            `INSERT INTO books (title, author, publisher, publication, page_count, work_key, isbn)
        VALUES ($1,$2,$3,$4,$5,$6,$7)
        RETURNING id`,
-      [
-        saveData.title,
-        saveData.authors,
-        saveData.publisher,
-        saveData.year,
-        saveData.page_count,
-        saveData.work_key,
-        saveData.isbn
-      ]
-    );
+            [
+                saveData.title,
+                saveData.authors,
+                saveData.publisher,
+                saveData.year,
+                saveData.page_count,
+                saveData.work_key,
+                saveData.isbn
+            ]
+        );
 
-    const bookId = bookResult.rows[0].id;
+        const bookId = bookResult.rows[0].id;
 
-    await db.query(
-      "INSERT INTO notes (book_id, content) VALUES ($1, $2)",
-      [bookId, saveData.notes]
-    );
+        await db.query(
+            "INSERT INTO notes (book_id, content) VALUES ($1, $2)",
+            [bookId, saveData.notes]
+        );
 
-    await db.query(
-      "INSERT INTO opinions (book_id, comment, score, date_read) VALUES ($1, $2, $3, $4)",
-      [bookId, saveData.comment, saveData.score, saveData.date_read + "-01"]
-    );
+        await db.query(
+            "INSERT INTO opinions (book_id, comment, score, date_read) VALUES ($1, $2, $3, $4)",
+            [bookId, saveData.comment, saveData.score, saveData.date_read + "-01"]
+        );
 
-    await db.query(
-      "INSERT INTO images (book_id, book_img) VALUES ($1, $2)",
-      [bookId, saveData.book_img]
-    );
+        await db.query(
+            "INSERT INTO images (book_id, book_img) VALUES ($1, $2)",
+            [bookId, saveData.book_img]
+        );
 
-    res.redirect("/");
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Server error");
-  }
+        res.redirect("/");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+    }
 });
 
 app.listen(port, () => {
